50ddde6ed33e80d87b6640f1738cfd4dfeb24072
Kit Taylor <kitling89@gmail.com>
2023-08-01 10:55:13 -0700

    Initial
    First 2 Kata

---
+++ Kata/DescendingOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kata
+{
+    public static class DescendingOrder
+    {
+        public static int ArrangeDescending(int num)
+        {
+            string numStr = num.ToString;
+            char[] digits = numStr.ToCharArray;
+
+            Array.Sort(digits);
+            Array.Reverse(digits);
+
+            string resultStr = new string(digits);
+            int    result    = int.Parse(resultStr);
+
+            return result;
+        }
+    }
+}
---
+++ Kata/GrasshopperSummation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public static class GrasshopperSummation
+    {
+        public static int summation(int num)
+        {
+            return Enumerable.Range(1, num).Sum();
+        }
+    }
+}
