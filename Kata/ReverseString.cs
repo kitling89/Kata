b0a23763f9f477cad9092fe51a5e9cf74ad7a4ee
Kit Taylor <kitling89@gmail.com>
2023-08-02 09:58:31 -0700

    8/2

---
+++ Kata/ReverseString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class ReverseString
+    {
+        public static string Solution(string str)
+        {
+            int    len       = str.Length;
+            char[] arraChars = str.ToCharArray();
+            string outp      = string.Empty;
+
+            for (int i = len - 1; i >= 0; i--)
+            {
+                outp += arraChars[i];
+            }
+
+            return outp;
+        }
+    }
+}
---
+++ Kata/StringRepeat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public static class StringRepeat
+    {
+        public static string RepeatStr(int n, string s)
+        {
+            string r = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                r += s;
+            }
+
+            return r;
+        }
+    }
+}
