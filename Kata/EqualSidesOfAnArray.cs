c3d893ed81bca216c2bb2809b6e5ed542aca1fd9
Kit Taylor <kitling89@gmail.com>
2023-08-03 14:47:31 -0700

    update

---
+++ Kata/EqualSidesOfAnArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Kata
+{
+    internal class EqualSidesOfAnArray
+    {
+        public static int FindEvenIndex(int[] arr)
+        {
+            int totalSum = 0;
+            foreach (int num in arr)
+            {
+                totalSum += num;
+            }
+
+            int leftSum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                totalSum -= arr[i];
+
+                if (leftSum == totalSum)
+                {
+                    return i;
+                }
+
+                leftSum += arr[i];
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Kata/Isograms.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class Isograms
+    {
+        public static bool IsIsogram(string str)
+        {
+            string lowStr      = str.ToLower();
+            char[] lowStrArray = lowStr.ToCharArray();
+
+            HashSet<char> seenChars  = new HashSet<char>();
+            bool          isRepeated = false;
+
+            foreach (char c in lowStrArray)
+            {
+                if (seenChars.Contains(c))
+                {
+                    isRepeated = true;
+                    break;
+                }
+                else
+                {
+                    seenChars.Add(c);
+                }
+            }
+
+            return !isRepeated;
+        }
+    }
+}
