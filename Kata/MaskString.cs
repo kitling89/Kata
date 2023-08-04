b9a7bc28d5dcda820a1dbd452c2d2d6458790f60
Kit Taylor <kitling89@gmail.com>
2023-08-04 08:27:56 -0700

    today

---
+++ Kata/MaskString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Kata
+{
+    internal class MaskString
+    {
+        // return masked string
+        public static string Maskify(string cc)
+        {
+            if (cc == null || cc.Length <= 4)
+            {
+                return cc;
+            }
+
+            int    maskedLength = cc.Length                     - 4;
+            string maskedString = new string('#', maskedLength) + cc.Substring(maskedLength);
+            return maskedString;
+        }
+    }
+}
