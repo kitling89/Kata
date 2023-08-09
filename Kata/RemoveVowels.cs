50d1a0f6237e625736cb444bf34f1d62538a48d3
Kit Taylor <kitling89@gmail.com>
2023-08-08 20:40:08 -0700

    88 update

---
+++ Kata/RemoveVowels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class RemoveVowels
+    {
+        public static string Disemvowel(string str)
+        {
+            string vowels = "AEIOUaeiou";
+            string result = "";
+
+            foreach (char c in str)
+            {
+                if (!vowels.Contains(c))
+                {
+                    result += c;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Kata/SquareSumed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class SquareSumed
+    {
+        public static int SquareSum(int[] numbers)
+        {
+            return numbers.Sum(number => number * number);
+        }
+    }
+}
