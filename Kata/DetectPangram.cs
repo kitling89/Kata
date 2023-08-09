a5fa96feea60f5b9e8848af80c1e0b6d0a5b2f26
Kit Taylor <kitling89@gmail.com>
2023-08-09 08:01:30 -0700

    today

---
+++ Kata/DetectPangram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class DetectPangram
+    {
+        public static bool IsPangram(string str)
+        {
+            string strLwr  = str.ToLower();
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+
+            foreach (char c in strLwr)
+            {
+                if (letters.Contains(c))
+                {
+                    letters = letters.Replace(c.ToString(), "");
+                }
+            }
+
+            if (letters.Length > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
