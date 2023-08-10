68ea9dab37fb1281d7149c9fcbf87451c29aca96
Kit Taylor <kitling89@gmail.com>
2023-08-10 08:16:15 -0700

    milk

---
+++ Kata/ArraySum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class ArraySum
+    {
+        public static double SumArray(double[] array)
+        {
+            return array.Sum();
+        }
+    }
+}
---
+++ Kata/PosandNegatives.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Kata
+{
+    internal class PosandNegatives
+    {
+        public static int[] CountPositivesSumNegatives(int[] input)
+        {
+            if (input == null || !input.Any())
+            {
+                return new int[] { };
+            }
+
+            int countPositives = input.Count(n => n > 0);
+            int sumNegatives   = input.Where(n => n < 0).Sum();
+
+            return new int[] { countPositives, sumNegatives };
+        }
+    }
+}
