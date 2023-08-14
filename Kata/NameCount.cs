d86a1128cdbe619cd5fc5575b7dd110df8b25423
Kit Taylor <kitling89@gmail.com>
2023-08-14 10:11:30 -0700

    thismorning

---
+++ Kata/NameCount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class NameCount
+    {
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            List<string> friends = new List<string>();
+            foreach (string name in names)
+            {
+                if (name.Length == 4)
+                {
+                    friends.Add(name);
+                }
+            }
+
+            return friends.ToArray();
+        }
+    }
+}
---
+++ Kata/TwotoOne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class TwotoOne
+    {
+        public static string Longest(string s1, string s2)
+        {
+            string s3Str    = s1 + s2;
+            string s3StrDis = new string(s3Str.Distinct().OrderBy(c => c).ToArray());
+            return (s3StrDis);
+        }
+    }
+}
