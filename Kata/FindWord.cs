3a9c79e8eb751d05e3094454c546604d6bbe0939
Kit Taylor <kitling89@gmail.com>
2023-08-15 09:04:05 -0700

    words

---
+++ Kata/FindWord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class FindWord
+    {
+        public static string FindNeedle(object[] haystack)
+        {
+            int pos = 0;
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] == "needle")
+                {
+                    pos = i;
+                }
+            }
+
+            return "found needle at position " + pos;
+        }
+    }
+}
---
+++ Kata/FirstLast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class FirstLast
+    {
+        public static string Remove_char(string s)
+        {
+            return s.Substring(1, s.Length - 2);
+        }
+    }
+}
