d20eb6e5c8f9360799e89c9a4fc5b06ca6f827ac
Kit Taylor <kitling89@gmail.com>
2023-08-19 10:15:49 -0700

    hazy saturday

---
+++ Kata/NameRecog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class NameRecog
+    {
+        public static string AreYouPlayingBanjo(string name)
+        {
+            char[] nameChars = name.ToLower().ToCharArray();
+            if (nameChars[0] == 'r')
+            {
+                return name + " plays banjo";
+            }
+
+            else return name + "does not play banjo";
+        }
+    }
+}
---
+++ Kata/Negative.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class Negative
+    {
+        public static int MakeNegative(int number)
+        {
+            if (number <= 0)
+                return number;
+            else
+                return number * -1;
+        }
+    }
+}
