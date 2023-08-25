1228355910345161930963c06067ec692950cc41
Kit Taylor <kitling89@gmail.com>
2023-08-25 10:01:23 -0700

    its friday

---
+++ Kata/OddEven.cs
@@ -0,0 +1,21 @@
+﻿namespace Kata;
+
+public class OddEven
+{
+    public static string OddOrEven(int[] array)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+
+        if (sum % 2 == 0)
+            return "even";
+
+        else
+        {
+            return "odd";
+        }
+    }
+}
---
+++ Kata/Paperworks.cs
@@ -0,0 +1,12 @@
+﻿namespace Kata;
+
+public class Paperworks
+{
+    public static int Paperwork(int n, int m)
+    {
+        if (n <= 0 ||m <= 0)
+            return 0;
+
+        return n * m;
+    }
+}
