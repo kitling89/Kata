fc0e2276aa003289779a0a3abfd6f811f4c8e2cb
Kit Taylor <kitling89@gmail.com>
2023-08-29 10:41:20 -0700

    starting ranking system

---
+++ Kata/RankingSystem.cs
@@ -0,0 +1,34 @@
+﻿namespace Kata;
+
+public class RankingSystem
+{
+    public class User
+    {
+        public int rank;
+        public int progress;
+
+        //initial rank -8
+        //ranks to +8 skipping 0
+        //100 progress per rank, carries over after rank-up
+        //higher level activies gain progress at rate: 10 * d * d where d is the difference between activity rank and user rank
+        //completing activity ranked same as user worth 3 points
+        //completing activity ranked one lower worth 1 point
+        //ignore activities 2 or more ranks lower
+
+        public void incProgress(int actRank)
+        {
+            //check if rank is 8
+
+            //if rank 
+                //2 or lower > no progress
+                //1 lower > 1 progress
+                //same > 3 progress
+                //more > 10 * d * d
+
+            //check if level up
+                //if level up modify rank and reset progress with carry over
+                //skip 0
+        }
+
+    }
+}
---
+++ Kata/UniqueOrder.cs
@@ -0,0 +1,22 @@
+﻿namespace Kata;
+
+public class UniqueOrder
+{
+    public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+    {
+        var list = new List<T>();
+        T cache = default(T);
+
+        foreach (var item in iterable)
+        {
+            if (!EqualityComparer<T>.Default.Equals(item, cache))
+            {
+                list.Add(item);
+                cache = item;
+            }
+        }
+
+        return list;
+
+    }
+}
