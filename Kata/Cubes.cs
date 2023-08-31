d4e3a60a81b9c6b9ce16f3757bc2899a82bcc749
Kit Taylor <kitling89@gmail.com>
2023-08-31 11:16:56 -0700

    clear

---
+++ Kata/Cubes.cs
@@ -0,0 +1,19 @@
+﻿namespace Kata;
+
+public class Cubes
+{
+    public static long CalculateCubeSum(long n)
+    {
+        return (n * (n + 1) / 2) * (n * (n + 1) / 2);
+    }
+    public static long findNb(long m)
+    {
+        long n = 1;
+        while (CalculateCubeSum(n) < m)
+        {
+            n++;
+        }
+
+        return CalculateCubeSum(n) == m ? n : -1;
+    }
+}
