1cb2ee5170632c726a545e9b52855bd8157417a4
Kit Taylor <kitling89@gmail.com>
2023-09-01 07:49:15 -0700

    friday!

---
+++ Kata/SumNumbers.cs
@@ -0,0 +1,29 @@
+﻿namespace Kata;
+
+public class SumNumbers
+{
+    public int GetSum(int a, int b)
+    {
+        int sum = 0;
+        if (a < b)
+        {
+            for (int i = a; i <= b; i++)
+            {
+                sum += i;
+            }
+        }
+        else if (a > b)
+        {
+            for (int i = b; i <= a; i++)
+            {
+                sum += i;
+            }
+        }
+        else
+        {
+            return a;
+        }
+        
+        return sum;
+    }
+}
