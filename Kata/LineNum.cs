9a970af7abe363f73c09f84822698baf6f5effdb
Kit Taylor <kitling89@gmail.com>
2023-11-13 10:09:20 -0800

    pump

---
+++ Kata/LineNum.cs
@@ -0,0 +1,10 @@
+﻿namespace Kata
+{
+    public class LineNum
+    {
+        public static List<string> Number(List<string> lines)
+        {
+            return lines.Select((item, index) => $"{index + 1}: {item}").ToList();
+        }
+    }
+}
